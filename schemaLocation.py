845406f269cf8c1d867a289572f77e44df3a82ac
jw <jakob.mikael.kruse-jensen.wrigley@regionh.dk>
2024-07-11 14:58:20 +0200

    collect errors at end

--- schemaLocation.py
+++ schemaLocation.py
@@ -6,6 +6,13 @@ import os
 from urllib.parse import urlparse
 
 cache = {}
+errors = []
+
+
+class error:
+    def __init__(self, url: str, status: int):
+        self.url = url
+        self.status = status
 
 
 def localdir(url: str) -> str:
@@ -24,13 +31,16 @@ def fix(url: str, page: str) -> str:
         if loc.startswith("http"):
             link = localdir(loc)
             curdir = localdir(url)
-            page = page.replace(loc, os.path.relpath(link, curdir))
+            page = page.replace(loc, f"{os.path.relpath(link, curdir)}/{os.path.basename(loc)}")
     return page
 
 
 def GET(url: str) -> str:
     if cache.get(url, None) is None:
-        cache[url] = requests.get(url).content.decode("utf-8")
+        req = requests.get(url)
+        if req.status_code > 220:
+            errors.append(error(url, req.status_code))
+        cache[url] = req.content.decode("utf-8")
     return cache[url]
 
 
@@ -39,22 +49,37 @@ def DL(url: str) -> str:
     dir = f"xsd/{uri.hostname}/{os.path.dirname(uri.path)}"
     if not os.path.exists(dir):
         os.makedirs(dir)
-    f = open(f"{dir}/{os.path.basename(uri.path)}", "w")
     page = fix(url, GET(url))
+    f = open(f"{dir}/{os.path.basename(uri.path)}", "w")
     f.write(page)
+    f.close()
     return GET(url)
 
 
 def get(url: str, depth: int):
     print(depth*' ' + url)
-    page = DL(url)
-    locations = grepLocation(page)
-    for loc in locations:
-        if not loc.startswith("http"):
-            dir = os.path.dirname(url)
-            loc = dir + "/" + loc
-        get(loc, depth+1)
+    try:
+        page = DL(url)
+        locations = grepLocation(page)
+        for loc in locations:
+            if not loc.startswith("http"):
+                dir = os.path.dirname(url)
+                loc = dir + "/" + loc
+            get(loc, depth+1)
+    except KeyboardInterrupt:
+        errors.append(error(url,0))
+        raise
+    except:
+        errors.append(error(url, 0))
 
 
 if __name__ == "__main__":
-    get(sys.argv[1], 0)
+    try:
+        get(sys.argv[1], 0)
+    except KeyboardInterrupt:
+        ...
+    finally:
+        if len(errors) > 0:
+            print("Errors:")
+            for e in errors:
+                print(f"{e.status}: {e.url}")
